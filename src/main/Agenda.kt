4f431bad8031f3be39a6fa792905c1142b82f7d0
Aryosvalldo Cleef de Souza <32641136+cleefsouza@users.noreply.github.com>
2021-12-11 01:01:32 -0300

    Condicionais

--- src/main/Agenda.kt
+++ src/main/Agenda.kt
@@ -4,7 +4,7 @@ import java.util.*
 
 fun main() {
     var nome: String;
-    var email: String;
+    var email: String? = null;
     var idade: Int;
 
 //    var input: Scanner = Scanner(System.`in`);
@@ -16,10 +16,23 @@ fun main() {
     nome = readLine().toString();
 
     print("Email: ")
-    email = readLine().toString();
+    email = readLine().toString() ?: "Email não informado";
 
     print("Idade: ")
     idade = readLine().toString().toInt();
 
     println("Contato de $nome salvo com sucesso!");
+
+    if (idade <= 17) {
+        println("$nome é menor de idade!");
+    } else if (idade in 18 .. 60) {
+        println("$nome é adulto!");
+    } else {
+        println("$nome é idoso!");
+    }
+
+    // println("Lenght: ${nome.length}");
+    // println("Equals: ${nome.equals("Cleef")}");
+    // println("Reversed: ${nome.reversed()}");
+    // println("Slice: ${nome.slice(1..3)}");
 }
